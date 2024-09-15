8d92d4d7381d1215ca53ae8adeb0186d0888f7da
leandrolimadeveloper <leandroslimasr@gmail.com>
2024-09-15 19:18:03 -0300

    Addon de acessibilidade

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -13,6 +13,7 @@ const config: StorybookConfig = {
   stories: ["../src/pages/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     getAbsolutePath("@storybook/addon-onboarding"),
+    getAbsolutePath("@storybook/addon-a11y"),
     getAbsolutePath("@storybook/addon-links"),
     getAbsolutePath("@storybook/addon-essentials"),
     getAbsolutePath("@chromatic-com/storybook"),
