ff58f511aa53029d7794545c6ca5466f84c0e7ad
leandrolimadeveloper <leandroslimasr@gmail.com>
2024-09-14 23:54:12 -0300

    Componente: Box

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,15 +1,1 @@
-import { styled } from './styles'
-
-export const Button = styled('button', {
-  fontFamily: '$default',
-  backgroundColor: '$ignite500',
-  borderRadius: '$md',
-  padding: '$2 $4',
-  border: 0,
-  fontWeight: 'bold',
-  color: '$white',
-})
-
-export function App() {
-  return <Button>Hello world</Button>
-}
+export * from './components/Box'
