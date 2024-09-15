37c86edcb007b88fbf66fd073ff9e11ea75475e8
leandrolimadeveloper <leandroslimasr@gmail.com>
2024-09-14 21:10:53 -0300

    Criando app em Storybook

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,10 +1,13 @@
 import { styled } from './styles'
 
-const Button = styled('button', {
+export const Button = styled('button', {
   fontFamily: '$default',
   backgroundColor: '$ignite500',
   borderRadius: '$md',
-  padding: '$4',
+  padding: '$2 $4',
+  border: 0,
+  fontWeight: 'bold',
+  color: '$white',
 })
 
 export function App() {
